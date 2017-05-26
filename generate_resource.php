0664785d45b2c43ca984c76ee291a7e4edecec92
bfgball <leehanchi@hotmail.com>
2017-05-26 10:08:16 +0800

    try preload with ccs

---
+++ generate_resource.php
@@ -0,0 +1,38 @@
+		<?php
+		header('content-type:text/html;charset=utf-8');
+		$path = 'games';
+		$resource = NULL;
+		function loadResource($path){
+			$handle = opendir($path);
+			global $resource;
+			while(($item = readdir($handle))!==false){
+				if($item != '.' && $item != '..'){
+					if(is_file($path . '/' . $item)){
+						if (pathinfo($path . '/' . $item,PATHINFO_EXTENSION) == 'png')
+						{
+							$resource[] = $path . '/' . $item;
+						}
+					} else {
+						$func = __FUNCTION__;
+						$func($path . '/' . $item);
+					}
+				}
+			}
+			closedir($handle);
+		}
+		loadResource($path);
+
+		// foreach ($resource as $key => $value) {
+		// 	echo '"';
+		// 	echo $value;
+		// 	echo '"';
+		// 	echo ',';
+		// 	echo '<br>';
+		// }
+
+		foreach ($resource as $key => $value) {
+			echo 'background-image: url(';
+			echo $value;
+			echo ');';
+			echo '<br>';
+		}
